d6c9fbcf3b1cae0a4e75d92fa0050cd1a03a0fd8
Durban G. Keeler <durban.keeler@gmail.com>
2020-04-15 13:05:55 -0600

    Converts to VSCode notebook cells; adds geoviews plots

--- notebooks/trend_plots.py
+++ notebooks/trend_plots.py
@@ -1,6 +1,9 @@
-# An exploratory notebook for recreating plots and 
-# graphics for the spatiotemporal trends in accumulation
+# %% [markdown]
+# # NOTEBOOK
+# 
+# An exploratory notebook for recreating plots and graphics for the spatiotemporal trends in accumulation
 
+# %%
 # Import modules
 import numpy as np
 import pandas as pd
@@ -11,6 +14,8 @@ from pathlib import Path
 # Define project root directory
 ROOT_DIR = Path(__file__).parent.parent
 
+
+# %%
 # Import R-generated accumulation and core data
 accum_long = pd.read_csv(
     ROOT_DIR.joinpath('data/R-trends/accum.csv')).drop(
@@ -25,3 +30,31 @@ core_sites = core_groups.mean().drop('Year', axis=1)
 
 trace_groups = accum_long.groupby('trace_ID')
 accum_sites = trace_groups.mean().drop('Year', axis=1)
+
+sites_gdf = gpd.GeoDataFrame(
+    accum_sites, 
+    geometry=gpd.points_from_xy(accum_sites.Easting, 
+    accum_sites.Northing), crs='EPSG:3031')
+
+cores_gdf = gpd.GeoDataFrame(
+    core_sites, 
+    geometry=gpd.points_from_xy(core_sites.Easting, 
+    core_sites.Northing), 
+    crs='EPSG:3031')
+
+# %%
+import geoviews as gv
+from cartopy import crs as ccrs
+from bokeh.io import output_notebook
+output_notebook()
+gv.extension('bokeh')
+
+ANT_proj = ccrs.SouthPolarStereo(true_scale_latitude=-71)
+
+accum_plt = gv.Points(sites_gpd.sample(1000),vdims=['accum'], 
+    crs=ANT_proj).opts(projection=ANT_proj, color='accum', 
+    cmap='plasma', colorbar=True
+)
+
+
+# %%
