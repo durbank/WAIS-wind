72f33b189f4d3273e232c6e97734208188406033
dejaentendu707 <32939754+dejaentendu707@users.noreply.github.com>
2020-04-15 10:10:24 -0600

    Updated downscaling
    My laptop crapped out on me in the middle of the night, so the downscaling results still haven't been QC'd. I'm rerunning now, but it should be done within an hour. I'll look through the results and forward them to you if everything looks ok. Best guess right now is that it takes about an hour and a half to run on my low-end laptop. I'm sure there's lots of room to speed that up, even if it doesn't happen for this class project.

--- src/Downscale_ERA5.py
+++ src/Downscale_ERA5.py
@@ -7,7 +7,37 @@ Downscale lower resolution ERA5 data to 8 m resolution using REMA DEM
 @author: Eric Johnson
 """
 
-def Downscale_ERA5(wind_u_10m, wind_v_10m, precip, vLat_ERA, vLon_ERA, vTime):
+# Load trace lat/lons
+
+# Import all required modules and variables
+from myModule import *
+import Load_ERA5 # *******Move to myModule**********
+import utm
+import numpy as np
+from geopy.distance import distance
+from numpy.matlib import repmat
+
+# Import PAIPR-generated data
+PAIPR_dir = ROOT_DIR.joinpath('data/gamma_20111109')
+data_0 = import_PAIPR(PAIPR_dir)
+
+accum_long_df = format_PAIPR(data_0, start_yr=1979, end_yr=2009)
+
+accum_long_df = accum_long_df.drop_duplicates(subset=['trace_ID'], keep='first')
+
+accum_easting  = []
+accum_northing = []
+for x in range(len(accum_long_df)):
+    easting, northing, zone_number, zone_letter = utm.from_latlon(accum_long_df['Lat'].iloc[x], accum_long_df['Lon'].iloc[x])
+    accum_easting.append(easting)
+    accum_northing.append(northing)
+
+accum_long_df['Easting']  = accum_easting
+accum_long_df['Northing'] = accum_northing
+
+wind_u_10m, wind_v_10m, precip, vLat, vLon, vTime, elevations = Load_ERA5.Load_ERA5()
+
+def Downscale_ERA5(wind_u_10m, wind_v_10m, precip, elevations, vLat, vLon, vTime, accum_long_df):
     
     # Alternate idea for supplying fewer variables as inputs
     # class ERA5:
@@ -21,101 +51,73 @@ def Downscale_ERA5(wind_u_10m, wind_v_10m, precip, vLat_ERA, vLon_ERA, vTime):
     # ERA5_Data.vLon = vLon_ERA
     # ERA5_Data.time = vTime
 
-    import utm
-    import numpy as np
-
     #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     # Load ERA5 data
-    
-    # wind_u, wind_v, precip
-    # iLat_min, iLat_max, iLong_min, iLong_max
-    # Elevation_ERA5
 
-    mElevations_ERA5 = [[1,2,3], [4,5,6], [7,8,9]] # placeholder
-    
+    mElevations_ERA5 = elevations
     
     #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    # Load REMA DEM
-    # mDEM
-    # iLat_min, iLat_max, iLong_min, iLong_max
-    # iResolution
+    # Load trace data
 
-    mLat_REMA = 0 # placeholder
-    mLon_REMA = 0 # placeholder
-
-    vLat_REMA = mREMA_Lat[:,1] # placeholder
-    vLon_REMA = mREMA_Lon[1,:] # placeholder
-    
-    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    # Mask out areas with radar trace data
-    
-    mMask_ERA  = 0 # placeholder
-    mMask_REMA = 0 # placeholder
+    trace_data_df = accum_long_df.set_index('trace_ID')
+    trace_data_df['wind_u'] = ""
+    trace_data_df['wind_v'] = ""
+    trace_data_df['precip'] = ""
 
     #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     # Convert lat/lon to gridded UTM
     
-    # ERA5
-    mEasting_ERA = np.zeros([len(vLat_ERA),len(vLon_ERA)])
-    mNorthing_ERA = np.zeros([len(vLat_ERA),len(vLon_ERA)])
-        
-    for lat_idx in range(len(vLat_ERA)):
-        lat = vLat_ERA(lat_idx)
-        for lon_idx in range(len(vLon_ERA)):
-            lon = vLon_ERA(lon_idx)
-            easting, northing, zone_number, zone_letter = utm.from_latlon(lat, lon)
-            mEasting_ERA[lat_idx,lon_idx]  = easting
-            mNorthing_ERA[lat_idx,lon_idx] = northing
-            
-    # REMA
-    mEasting_REMA = np.zeros([len(vLat_REMA),len(vLon_REMA)])
-    mNorthing_REMA = np.zeros([len(vLat_REMA),len(vLon_REMA)])
-        
-    for lat_idx in range(len(vLat_REMA)):
-        lat = vLat_REMA(lat_idx)
-        for lon_idx in range(len(vLon_REMA)):
-            lon = vLon_REMA(lon_idx)
-            easting, northing, zone_number, zone_letter = utm.from_latlon(lat, lon)
-            mEasting_REMA[lat_idx,lon_idx]  = easting
-            mNorthing_REMA[lat_idx,lon_idx] = northing
+    mLat_ERA = np.transpose(repmat(vLat,len(vLon),1))
+    mLon_ERA = repmat(vLon,len(vLat),1)
 
     #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    # Downscale ERA at each point onto high resolution (DEM) grid
+    # Downscale ERA at each trace location
+
+    for x in range(len(trace_data_df)):
+        
+        try:
+            # Find closest ERA5 location to each trace ID
+            closest_idx = np.sqrt(
+                np.square(np.matrix(mLat_ERA - trace_data_df['Lat'][x])) + 
+                np.square((np.matrix(mLon_ERA - trace_data_df['Lon'][x])))
+                             ).argmin()
+            closest_idx = np.unravel_index(closest_idx, np.shape(mLat_ERA))
             
-    mWindU_hd  = np.zeros(np.shape(mEasting_REMA))
-    mWindV_hd  = np.zeros(np.shape(mEasting_REMA))
-    mPrecip_hd = np.zeros(np.shape(mEasting_REMA))
-
-    for m in range(np.shape(mEasting_REMA)[0]):
-        for n in range(np.shape(mEasting_REMA)[1]):
-            try: # try/except necessary to avoid edges of ERA5 data matrix
+            # Calculate distances to nearest ERA5 grid points
+            m3x3_ERA_Lat = vLat[closest_idx[0]-1:closest_idx[0]+2]
+            m3x3_ERA_Lat = np.transpose(repmat(m3x3_ERA_Lat,3,1))
+            m3x3_ERA_Lon  = vLon[closest_idx[1]-1:closest_idx[1]+2]
+            m3x3_ERA_Lon = repmat(m3x3_ERA_Lon,3,1)
+            m3x3_Distances = np.zeros([3,3])
+            for m in range(3):
+                for n in range(3):
+                    era_lat = m3x3_ERA_Lat[m,n]
+                    era_lon = m3x3_ERA_Lon[m,n]
+                    trace_lat = trace_data_df['Lat'][x]
+                    trace_lon = trace_data_df['Lon'][x]
+                    m3x3_Distances[m,n] = distance([era_lat,era_lon], [trace_lat, trace_lon]).km
+            m3x3_Weights = m3x3_Distances / np.sum(m3x3_Distances[:])
+            # Calculate weighted mean (based on distance) of surrounding ERA5 values
+            # for each time step
+            vWind_u = []
+            vWind_v = []
+            vPrecip = []
+            for y in range(np.shape(wind_u_10m)[0]):
+                temp = m3x3_Weights * wind_u_10m[y,closest_idx[0]-1:closest_idx[0]+2,closest_idx[1]-1:closest_idx[1]+2]
+                vWind_u.append(np.sum(temp[:]))
+                temp = m3x3_Weights * wind_v_10m[y,closest_idx[0]-1:closest_idx[0]+2,closest_idx[1]-1:closest_idx[1]+2]
+                vWind_v.append(np.sum(temp[:]))
+                temp = m3x3_Weights * precip[y,closest_idx[0]-1:closest_idx[0]+2,closest_idx[1]-1:closest_idx[1]+2]
+                vPrecip.append(np.sum(temp[:]))
                 
-                # Find indices of nearest ERA grid point
-                iERA_e_idx = (np.abs(mEasting_ERA  -  mEasting_REMA(m,n))).argmin() # does this find index or value? want index
-                iERA_n_idx = (np.abs(mNorthing_ERA - mNorthing_REMA(m,n))).argmin() # does this find index or value? want index
-                # Calculate distances to nearest ERA5 grid points
-                m3x3_ERA_Easting  =  mEasting_ERA[iERA_e_idx-1:iERA_e_idx+1,iERA_n_idx-1:iERA_n_idx+1]
-                m3x3_ERA_Northing = mNorthing_ERA[iERA_e_idx-1:iERA_e_idx+1,iERA_n_idx-1:iERA_n_idx+1]
-                m3x3_Distances = np.sqrt( (m3x3_ERA_Easting - mEasting_REMA[m,n])**2 + (m3x3_ERA_Northing - mNorthing_REMA[m,n])**2 )
-                m3x3_Weights = m3x3_Distances / sum(m3x3_Distances[:])
-                # Calculate weighted mean (based on distance) of surrounding ERA5 values
-                temp = m3x3_Weights * wind_u_10m[iERA_e_idx-1:iERA_e_idx+1,iERA_n_idx-1:iERA_n_idx+1]
-                mWindU_hd[m,n] = sum(temp[:])
-                temp = m3x3_Weights * wind_v_10m[iERA_e_idx-1:iERA_e_idx+1,iERA_n_idx-1:iERA_n_idx+1]
-                mWindV_hd[m,n] = sum(temp[:])
-                temp = m3x3_Weights * precip[iERA_e_idx-1:iERA_e_idx+1,iERA_n_idx-1:iERA_n_idx+1]
-                mPrecip_hd[m,n] = sum(temp[:])
-                
-            except:
-                pass
+            trace_data_df['wind_u'][x] = vWind_u
+            trace_data_df['wind_v'][x] = vWind_v
+            trace_data_df['precip'][x] = vPrecip
+            
+        except:
+            pass
 
-
-
-
-
-
-
-
+        print([x/39024*100,'%'])
 
 
 
